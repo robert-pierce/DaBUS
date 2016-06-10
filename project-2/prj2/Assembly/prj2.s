dd05561db4d8da7dc7d5480d6f23589dcc665150
robert-pierce <rpierce3@gmail.com>
2016-06-09 22:04:03 -0400

    program tested without interrupts ... all good

--- project-2/prj2/Assembly/prj2.s
+++ project-2/prj2/Assembly/prj2.s
@@ -39,25 +39,27 @@ vector0:    .fill 0x00000000 !0
             .fill 0x00000000 !15
 !end vector table
 
-main:           la $sp, stack           ! Initialize stack pointer
-                lw $sp, 0($sp)          
+main:           la      $sp, stack           ! Initialize stack pointer
+                lw      $sp, 0($sp)          
                 
-                ! Install timer interrupt handler into vector table
-                noop                    ! FIX ME
-                ei                      ! Don't forget to enable interrupts...
+                ! Install timer interrupt handler into vector table              
+                la      $a0, ti_inthandler   ! get address of interrupt handler for timer
+                sw      $a0, 1($zero)        ! load address of interrupt handler into 0x00000001
+        
+                ei                           ! Don't forget to enable interrupts...
 
-                la $at, factorial       ! load address of factorial label into $at
-                addi $a0, $zero, 5      ! $a0 = 5, the number to factorialize
-                jalr $at, $ra           ! jump to factorial, set $ra to return addr
-                halt                    ! when we return, just halt
+                la      $at, factorial       ! load address of factorial label into $at
+                addi    $a0, $zero, 5        ! $a0 = 5, the number to factorialize
+                jalr    $at, $ra             ! jump to factorial, set $ra to return addr
+                halt                         ! when we return, just halt
 
-factorial:      addi    $sp, $sp, -1    ! push RA
+factorial:      addi    $sp, $sp, -1         ! push RA
                 sw      $ra, 0($sp)
-                addi    $sp, $sp, -1    ! push a0
+                addi    $sp, $sp, -1         ! push a0
                 sw      $a0, 0($sp)
-                addi    $sp, $sp, -1    ! push s0
+                addi    $sp, $sp, -1         ! push s0
                 sw      $s0, 0($sp)
-                addi    $sp, $sp, -1    ! push s1
+                addi    $sp, $sp, -1         ! push s1
                 sw      $s1, 0($sp)
 
                 beq     $a0, $zero, base_zero
@@ -71,35 +73,96 @@ factorial:      addi    $sp, $sp, -1    ! push RA
     base_one:   addi    $v0, $zero, 1   ! 1! = 1
                 beq     $zero, $zero, done
 
-    recurse:    add     $s1, $a0, $zero     ! save n in s1
-                addi    $a0, $a0, -1        ! n! = n * (n-1)!
+    recurse:    add     $s1, $a0, $zero      ! save n in s1
+                addi    $a0, $a0, -1         ! n! = n * (n-1)!
                 la      $at, factorial
                 jalr    $at, $ra
 
-                add     $s0, $v0, $zero     ! use s0 to store (n-1)!
-                add     $v0, $zero, $zero   ! use v0 as sum register
-        mul:    beq     $s1, $zero, done    ! use s1 as counter (from n to 0)
+                add     $s0, $v0, $zero      ! use s0 to store (n-1)!
+                add     $v0, $zero, $zero    ! use v0 as sum register
+    mul:        beq     $s1, $zero, done     ! use s1 as counter (from n to 0)
                 add     $v0, $v0, $s0
                 addi    $s1, $s1, -1
                 beq     $zero, $zero, mul
 
-    done:       lw      $s1, 0($sp)     ! pop s1
+    done:       lw      $s1, 0($sp)          ! pop s1
                 addi    $sp, $sp, 1
-                lw      $s0, 0($sp)     ! pop s0
+                lw      $s0, 0($sp)          ! pop s0
                 addi    $sp, $sp, 1
-                lw      $a0, 0($sp)     ! pop a0
+                lw      $a0, 0($sp)          ! pop a0
                 addi    $sp, $sp, 1
-                lw      $ra, 0($sp)     ! pop RA
+                lw      $ra, 0($sp)          ! pop RA
                 addi    $sp, $sp, 1
                 jalr    $ra, $zero
 
-ti_inthandler:
-    noop        ! FIXME
+ ti_inthandler: addi    $sp, $sp, -1         ! push $k0 onto stack
+                sw      $k0,    0($sp)
+
+                ei                           ! enable interrupts
+        
+                addi    $sp, $sp, -1         ! push $ra
+                sw      $ra, 0($sp)
+                addi    $sp, $sp, -1         ! push $a0
+                sw      $a0, 0($sp)
+                addi    $sp, $sp, -1         ! push $s0
+                sw      $s0, 0($sp)
+                addi    $sp, $sp, -1         ! push $s1
+                sw      $s1, 0($sp)
+                addi    $sp, $sp, -1         ! push $v0
+                sw      $v0, 0($sp)
+                addi    $sp, $sp, -1         ! push $at
+                sw      $at, 0($sp)
 
 
+!!! interrupt code
+                la      $a0, seconds          ! load seconds
+                lw      $a0, 0($a0)
+        
+                la      $a1, minutes          ! load minutes
+                lw      $a1, 0($a1)
+
+                la      $a2, hours            ! load hours
+                lw      $a2, 0($a2)
+
+                addi    $a3, $zero, 59        ! load 59 into $a3
 
 
+        
+                beq     $a0, $a3, inc_minutes  
+                addi    $a0, $a0, 1          ! inc_seconds
+                beq     $zero, $zero,  clean_up
 
+   inc_minutes: add     $a0, $zero, $zero    ! reset seconds to zero
+                beq     $a1, $a3,  inc_hours
+                addi    $a1, $a1,  1         ! inc minutes
+                beq     $zero, $zero, clean_up
+
+    inc_hours:  add     $a1, $zero, $zero    ! reset minutes
+                addi    $a2, $a2,  1         ! inc hours
+
+    clean_up:   lw      $at, 0($sp)          ! pop $at from the stack
+                addi    $sp, $sp, 1    
+                lw      $v0, 0($sp)          ! pop $v0
+                addi    $sp, $sp, 1      
+                lw      $s1, 0($sp)          ! pop $s1
+                addi    $sp, $sp, 1    
+                lw      $s0, 0($sp)          ! pop $s0
+                addi    $sp, $sp, 1
+                lw      $a0, 0($sp)          ! pop $a0
+                addi    $sp, $sp, 1    
+                sw      $ra, 0($sp)          ! pop $ra
+                addi    $sp, $sp, 1
+
+                di                           ! disable interrupts
+        
+                sw      $k0, 0($sp)          ! pop $k0
+                addi    $sp, $sp, 1
+
+                reti                         ! reurn from interrupt
+                
+
+
+        
 
 
 
